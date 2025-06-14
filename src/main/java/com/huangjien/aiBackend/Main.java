a67ca408d6ac760424e296432efabed832f34119
huangjien <huangjien@gmail.com>
2025-06-14 09:11:02 +0100

    init

---
+++ src/main/java/com/huangjien/aiBackend/Main.java
@@ -0,0 +1,17 @@
+package com.huangjien.aiBackend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        logger.info("Hello and welcome to AI Backend!");
+
+        SpringApplication.run(Main.class, args);
+    }
+}
